e6a862a1ae3a5d1ed5cf4bba1c06434f1e8ef79a
Alexander Curtis <alexander@Alexanders-MacBook-Pro.local>
2024-07-21 16:40:04 +0100

    latest push before graphql experiment

--- packages/block-editor/src/hooks/useBlockEditor.ts
+++ packages/block-editor/src/hooks/useBlockEditor.ts
@@ -1,39 +1,53 @@
+import { useCallback } from 'react'
 import { Editor, useEditor } from '@tiptap/react'
 import { ExtensionKit } from '@/extensions/extension-kit'
 import { useSidebar } from './useSidebar'
 // import { initialContent } from '@/lib/data/initialContent' INITIAL CONTENT FILL
 
 declare global {
-  interface Window {
-    editor: Editor | null
-  }
+    interface Window {
+        editor: Editor | null
+    }
 }
 
 export const useBlockEditor = () => {
-  const leftSidebar = useSidebar()
-  const editor = useEditor(
-    {
-      autofocus: true,
-      onCreate: ({ editor }) => {
-      },
-      extensions: [
-        ...ExtensionKit({}),
-      ],
-      editorProps: {
-        attributes: {
-          autocomplete: 'off',
-          autocorrect: 'off',
-          autocapitalize: 'off',
-          class: 'min-h-full',
+
+    // Should this be useCallback
+    const onUpdate = useCallback((cb) => {
+
+    }, [])
+
+
+    const leftSidebar = useSidebar()
+    const editor = useEditor(
+        {
+            autofocus: true,
+            onCreate: ({ editor }) => {
+            },
+            onUpdate({ editor }) {
+                console.log('editor updated', editor.getJSON());
+            },
+            extensions: [
+                ...ExtensionKit({}),
+            ],
+            editorProps: {
+                attributes: {
+                    autocomplete: 'off',
+                    autocorrect: 'off',
+                    autocapitalize: 'off',
+                    class: 'min-h-full',
+                },
+            },
         },
-      },
-    },
-    [],
-  )
+        [],
+    )
 
-  const characterCount = editor?.storage.characterCount || { characters: () => 0, words: () => 0 }
 
-  window.editor = editor
+    // TODO - DO WE NEED A DESTRUCTOR HERE TO CLEANUP?
 
-  return { editor, characterCount, leftSidebar }
+    const characterCount = editor?.storage.characterCount || { characters: () => 0, words: () => 0 }
+
+    window.editor = editor
+
+    return { editor, characterCount, leftSidebar }
 }
