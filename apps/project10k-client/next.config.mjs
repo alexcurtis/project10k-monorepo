cc0c1843dc3c64f603b1850b6d9196d8195b574f
Alexander Curtis <alexander@Alexanders-MacBook-Pro.local>
2024-07-21 12:41:10 +0100

    refrences for monorepo setup

--- apps/project10k-client/next.config.mjs
+++ apps/project10k-client/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false, // Enable This When Checking For Mounting Problems
-    transpilePackages: ['@vspark/block-editor']
+    transpilePackages: ['@vspark/block-editor', '../../packages/block-editor']
 };
 
 export default nextConfig;
